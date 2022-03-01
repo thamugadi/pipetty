ebb95ee82e0196a2f0a28e538b4d60cc59925f23
thamugadi <qirimhangli@gmail.com>
2022-03-01 16:47:27 +0100

    makefile

---
+++ Makefile
@@ -0,0 +1,4 @@
+pipetty : main.c
+	gcc main.c -o pipetty
+	sudo chown root pipetty
+	sudo chmod +s pipetty
--- main.c
+++ main.c
@@ -4,21 +4,32 @@
 #include <stdlib.h>
 #include <fcntl.h>
 #include <sys/types.h>
+#include <termios.h>
 #include <unistd.h>
-void sendTTY(int ptty_fd, char* message, int len)
+int sendTTY(int ptty_fd, char* message, int len)
 {
-        for (int i = 0; i < len; i++) ioctl(ptty_fd, TIOCSTI, message+i);
+        int success;
+        for (int i = 0; i < len; i++) {
+                success = ioctl(ptty_fd, TIOCSTI, message+i);
+                if (success) return success;
+        }
+        return success;
 }
 
 int main(int argc, char** argv)
 {
-        if (argc != 3) printf("./program /dev/pts/N file\n");
-        else
-        {
-                int fd = open(argv[2], O_RDONLY);
-                int len = lseek(fd,0, SEEK_END);
-                char* data = mmap(0, len, PROT_READ, MAP_PRIVATE, fd, 0);
-                int pts_fd = open(argv[1], O_RDWR);
-                sendTTY(pts_fd, data, len);
+        if (argc != 3){
+                printf("./program /dev/pts/N file\n");
+                return -1;
         }
+        unsigned char clearSymbols[] = {0x1B,0x63,0x0A};
+        int fd = open(argv[2], O_RDONLY);
+        int len = lseek(fd,0, SEEK_END);
+        char* data = mmap(0, len, PROT_READ, MAP_PRIVATE, fd, 0);
+        int pts_fd = open(argv[1], O_RDWR);
+        int s;
+        s = sendTTY(pts_fd, data, len);
+        sleep(2);
+        write(pts_fd, clearSymbols, 3);
 }
+
